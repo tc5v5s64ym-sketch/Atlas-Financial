6612502cb0167f6aba112c362ee444ad28906ea0
tc5v5s64ym-sketch <tc5v5s64ym@privaterelay.appleid.com>
2026-08-25 19:32:46 -0700

    Register plain-language homepage acceptance

--- test.js
+++ test.js
@@ -48,6 +48,7 @@ const suites = [
   ['spending classification reconciliation', 'test-classification.js'],
   ['Lunch Money historical actuals authority (AF-OPERATE-01)', 'test-periods-lunchmoney.js'],
   ['decision-first payday operating surface (AF-OPERATE-02)', 'test-operating-surface.js'],
+  ['plain-language household homepage', 'test-household-view.js'],
   ['ordered payday action sheet (AF-OPERATE-03)', 'test-operate-payday-action-sheet.js'],
   ['between-paydays operating view (AF-OPERATE-04)', 'test-operate-between-paydays.js'],
   ['future financial gravity (AF-OPERATE-05)', 'test-operate-future-gravity.js'],
