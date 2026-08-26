8392d1ea86efc90627b519716a8abeed921ec55b
tc5v5s64ym-sketch <tc5v5s64ym@privaterelay.appleid.com>
2026-08-26 10:24:25 -0700

    Run cash waterfall acceptance

--- test.js
+++ test.js
@@ -50,6 +50,7 @@ const suites = [
   ['decision-first payday operating surface (AF-OPERATE-02)', 'test-operating-surface.js'],
   ['plain-language household homepage', 'test-household-view.js'],
   ['Plan homepage decision desk', 'test-plan-decision-desk.js'],
+  ['cash-waterfall household homepage', 'test-cash-waterfall-view.js'],
   ['ordered payday action sheet (AF-OPERATE-03)', 'test-operate-payday-action-sheet.js'],
   ['between-paydays operating view (AF-OPERATE-04)', 'test-operate-between-paydays.js'],
   ['future financial gravity (AF-OPERATE-05)', 'test-operate-future-gravity.js'],
